abeecdb005f1a353a86338cc8194d6f760241553
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-01-12 16:58:09 +0100

    update

---
+++ src/Es3/Main.java
@@ -0,0 +1,7 @@
+package Es3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Es3/entities/ContoOnline.java
@@ -0,0 +1,26 @@
+package Es3.entities;
+
+public class ContoOnline extends ContoCorrente {
+    private double maxPrelievo;
+
+    public ContoOnLine(String titolare, double saldo, double maxP) {
+        super(titolare, saldo);
+        this.maxPrelievo = maxP;
+    }
+
+    public void stampaSaldo() {
+        System.out.println(
+                "Titolare: " + titolare +
+                        " - Saldo: " + saldo +
+                        " - Num movimenti: " + nMovimenti +
+                        " - Massimo movimenti: " + maxMovimenti +
+                        " - Massimo prelievo possibile: " + maxPrelievo
+        );
+    }
+
+    public void preleva(double x) {
+        if (x <= maxPrelievo) {
+            super.preleva(x);
+        }
+    }
+}
